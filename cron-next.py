54d4cbf5a730505fe83db8076ed35c3ff69c327c
mohsenil85 <mohsenil85@gmail.com>
2013-05-03 15:36:12 -0600

    massaging text into a list

--- cron-next.py
+++ cron-next.py
@@ -1,10 +1,38 @@
-import sys, getopt
+import sys, datetime, re, time
+
+#def replace_stars(tup):
+
+def processlines(line):
+  if not line.startswith("#"):
+    line.replace("*", "0")
+    words = line.split()
+    l = list(words)
+    job = l[-1:]
+    print str(job)
+    l = l[:7]
+    l.reverse()
+
+    print l
+    #t = tuple(words)
+    #print t
+      
+now = datetime.datetime.now()
+
+
+
+
 
 def main(argv):
   infile = open(str(sys.argv[1]))
-  print "name: " , infile.name
+  #print "name: " , infile.name
+  lines = infile.readlines()
+
+  for line in lines:
+
+    processlines(line)
 
   infile.close()
 
 if __name__ == "__main__":
-     main(sys.argv[1:])
+  main(sys.argv[1:])
+
